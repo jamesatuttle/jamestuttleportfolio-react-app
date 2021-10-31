5edd23a452ce537f38b323f109a4028e24dfe570
James Tuttle <jamesatuttle95@gmail.com>
2021-10-31 21:22:24 +0000

    update project section styling

--- jamestuttleportfolio/src/components/views/Projects.js
+++ jamestuttleportfolio/src/components/views/Projects.js
@@ -3,32 +3,53 @@ import ChevronRight from '../svgs/ChevronRight';
 import Css3 from '../svgs/Css3';
 import Javascipt from '../svgs/Javascript';
 import Pencil from '../svgs/Pencil';
+import { Link } from 'react-router-dom';
 
 class Projects extends React.Component {
   render() {
     return (
       <section id='projects'>
         <h2>Projects</h2>
+        <div className='projectsIntro'>
+          <p>Take a look at the Projects I’ve been working on, from building simple to complex pictures using CSS, to seeing what I can make in JavaScript.</p>
+          <p>I’m always adding to this to keep coming back to check on what’s new!</p>
+        </div>
 
         <div className='projects_container'>
-          {/* <div className='project_tile'> */}
-              {/* <h3>Queery website design</h3> */}
-              {/* <Pencil /> */}
-          {/* </div> */}
           <div className='project_tile'>
-              <Javascipt /><h3><a href='#'>Random number generator</a></h3>
-              <p>A mini project which takes in a minimum number and a maximum number and returns a random number between. Built using JavaScript.</p>
+              <h3>Random number generator</h3><Javascipt/>
+              <p>September 2021</p>
+              <Link to='/projects/random-number-generator'>Take a look</Link>
           </div>
-          {/* <div className='project_tile'> */}
-              {/* <h3>Queery logo design</h3> */}
-              {/* <Pencil /> */}
-          {/* </div> */}
           <div className='project_tile'>
-              <Css3 /><h3><a href='#'>Drawings using css</a></h3>
-              <p>Some small drawings created using CSS to practice layering, ::before and ::after pseudo selectors. I’ve included the codepen links so you can see the code</p>
+              <h3>Drawings using css</h3><Css3/>
+              <p>September 2021</p>
+              <Link to='/projects/drawing-using-css'>Take a look</Link>
+          </div>
+          <div className='project_tile'>
+              <h3>Drawings using css</h3><Css3/>
+              <p>September 2021</p>
+              <Link to='/projects/drawing-using-css'>Take a look</Link>
+          </div>
+          <div className='project_tile'>
+              <h3>Drawings using css</h3><Css3/>
+              <p>September 2021</p>
+              <Link to='/projects/drawing-using-css'>Take a look</Link>
+          </div>
+          <div className='project_tile'>
+              <h3>Drawings using css</h3><Css3/>
+              <p>September 2021</p>
+              <Link to='/projects/drawing-using-css'>Take a look</Link>
+          </div>
+          <div className='project_tile'>
+              <h3>Drawings using css</h3><Css3/>
+              <p>September 2021</p>
+              <Link to='/projects/drawing-using-css'>Take a look</Link>
           </div>
         </div>
-        <a className='button_style primary' href='/projects'>see all projects</a>
+        <div className='buttonContainer'>
+          <a className='button_style primary' href='/projects'>see all projects</a>
+        </div>
       </section>
     );
   }
