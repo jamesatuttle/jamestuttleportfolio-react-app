4161adf652c3b4db3314fe50f0debcbe23a13726
James Tuttle <jamesatuttle95@gmail.com>
2021-10-31 19:30:42 +0000

    add styling for about section

--- jamestuttleportfolio/src/components/views/About.js
+++ jamestuttleportfolio/src/components/views/About.js
@@ -1,5 +1,8 @@
-import React from 'react'
-
+import React from 'react';
+import Graduate from '../svgs/Graduate';
+import Computer from '../svgs/Computer';
+import Swimming from '../svgs/Swimming';
+import Star from '../svgs/Star';
 
 class About extends React.Component {
 
@@ -7,8 +10,30 @@ class About extends React.Component {
     return (
       <section id='about'>
         <h2>About me</h2>
-        <p>I came to Newcastle for University and then stayed in the city for work. I’ve had a couple of jobs within tech, starting as an <span className='bigger'>Assistant Software Developer</span> for HMRC, a <span className='bigger'>Website Developer</span> then a <span className='bigger'>Junior Frontend Developer</span> and I currently work as a <span className='bigger'>Platform Support Engineer</span> for Partnerize.</p>
-        <p>Outside of work, I love keeping fit with swimming, the gym and rock climbing. I also love Console and PC gaming, playing Dungeons and Dragons, and travelling to experience the world and finding awesome street art.</p>
+
+        <Graduate/><h3 id='education'>Education</h3>
+        <h4>Northumbria University, Newcastle</h4>
+        <p>BSc. Computer Science 2013 - 2017</p>
+
+
+        <Computer/><h3 id='work'>Where I've Worked</h3>
+        <h4>HM Revenue &amp; Customs</h4>
+        <p>Assistant Software Developer 2015 - 2016</p>
+
+        <h4>Caspian</h4>
+        <p>Junior Frontend Developer 2018 - 2019</p>
+
+        <h4>Partnerize</h4>
+        <p>Application Support Engineer 2017 - 2018<br/>
+        Platform Support Engineer 2019 - Present</p>
+
+
+        <Swimming/><h3 id='hobbies'>Hobbies</h3>
+        <p>I love keeping fit with swimming, the gym, and rock climbing. I also love Console and PC gaming, playing Dungeons and Dragons, and travelling to experience the world and finding awesome street art.</p>
+
+        <Star/><h3 id='ambition'>Ambition</h3>
+        <p>My ambition is to focus primarily on Frontend Development with a bit of backend, maybe one day transitioning to a full stack developer.</p>
+
       </section>
     );
   }
