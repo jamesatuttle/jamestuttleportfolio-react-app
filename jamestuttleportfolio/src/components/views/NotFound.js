316059ef12e050b310207ecfe30799e306de5ffa
James Tuttle <jamesatuttle95@gmail.com>
2021-11-16 20:17:01 +0000

    build react package for deployment

--- jamestuttleportfolio/src/components/views/NotFound.js
+++ jamestuttleportfolio/src/components/views/NotFound.js
@@ -7,16 +7,11 @@ class NotFound extends React.Component {
   render() {
     return (
       <PageLayout page="notfound">
-        <div id='text-with-image'>
-          <div className='illustration'>
-            <img id='monitor_image' alt="monitor created using css" src={monitor}/>
-          </div>
-          <div id='text-container'>
+        <section>
             <h1>Page Not Found</h1>
             <p>It seems the page you're looking for doesn't exist or has been removed.<br/>Head home using the link below.</p>
-            <Link className='primary' to='/home'>Back to home</Link>
-          </div>
-        </div>
+            <Link className='link' to='/home'>Back to home</Link>
+        </section>
       </PageLayout>
     );
   }
